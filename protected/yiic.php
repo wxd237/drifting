24fd39c28417b7d32af390c041830e092fa8732d
Administrator <admin@local.host>
2013-11-16 15:22:48 +0800

    init

---
+++ protected/yiic.php
@@ -0,0 +1,7 @@
+<?php
+
+// change the following paths if necessary
+$yiic=dirname(__FILE__).'/../../yiisoft-yii-15a50d2/framework/yiic.php';
+$config=dirname(__FILE__).'/config/console.php';
+
+require_once($yiic);
