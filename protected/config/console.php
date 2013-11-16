6ae6ca084512b1e12b76c082a7713b02f6c933c7
Administrator <admin@local.host>
2013-11-16 16:25:17 +0800

    the 2 post

--- protected/config/console.php
+++ protected/config/console.php
@@ -11,19 +11,17 @@ return array(
 
 	// application components
 	'components'=>array(
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+		
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=drifting',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'w',
 			'charset' => 'utf8',
 		),
-		*/
+		
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
@@ -34,4 +32,38 @@ return array(
 			),
 		),
 	),
+	 'modules'=>array(
+            #...
+            'user'=>array(
+                # encrypting method (php hash function)
+                'hash' => 'md5',
+
+                # send activation email
+                'sendActivationMail' => true,
+
+                # allow access for non-activated users
+                'loginNotActiv' => false,
+
+                # activate user on registration (only sendActivationMail = false)
+                'activeAfterRegister' => false,
+
+                # automatically login from registration
+                'autoLogin' => true,
+
+                # registration path
+                'registrationUrl' => array('/user/registration'),
+
+                # recovery password path
+                'recoveryUrl' => array('/user/recovery'),
+
+                # login form path
+                'loginUrl' => array('/user/login'),
+
+                # page after login
+                'returnUrl' => array('/user/profile'),
+
+                # page after logout
+                'returnLogoutUrl' => array('/user/login'),
+            ),
+     ),
 );
--- protected/config/main.php
+++ protected/config/main.php
@@ -16,26 +16,63 @@ return array(
 	'import'=>array(
 		'application.models.*',
 		'application.components.*',
+		 'application.modules.user.models.*',
+            'application.modules.user.components.*',
 	),
 
 	'modules'=>array(
 		// uncomment the following to enable the Gii tool
-		/*
+		
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'w',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
-			'ipFilters'=>array('127.0.0.1','::1'),
+			'ipFilters'=>array('*','::1'),
 		),
-		*/
+		 'user'=>array(
+                # encrypting method (php hash function)
+                'hash' => 'md5',
+
+                # send activation email
+                'sendActivationMail' => false,
+
+                # allow access for non-activated users
+                'loginNotActiv' => false,
+
+                # activate user on registration (only sendActivationMail = false)
+                'activeAfterRegister' => true,
+
+                # automatically login from registration
+                'autoLogin' => true,
+
+                # registration path
+                'registrationUrl' => array('/user/registration'),
+
+                # recovery password path
+                'recoveryUrl' => array('/user/recovery'),
+
+                # login form path
+                'loginUrl' => array('/user/login'),
+
+                # page after login
+                'returnUrl' => array('/user/profile'),
+
+                # page after logout
+                'returnLogoutUrl' => array('/user/login'),
+             
+            ),
+		
 	),
 
 	// application components
 	'components'=>array(
-		'user'=>array(
-			// enable cookie-based authentication
-			'allowAutoLogin'=>true,
-		),
+
+		  'user'=>array(
+                // enable cookie-based authentication
+                'class' => 'WebUser',
+                'allowAutoLogin'=>true,
+                'loginUrl' => array('/user/login'),
+            ),
 		// uncomment the following to enable URLs in path-format
 		/*
 		'urlManager'=>array(
@@ -47,19 +84,17 @@ return array(
 			),
 		),
 		*/
-		'db'=>array(
-			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
-		),
+	
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=drifting',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => '',
+			'password' => 'w',
 			'charset' => 'utf8',
 		),
-		*/
+		
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
