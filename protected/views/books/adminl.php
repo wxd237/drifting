47b9cf76aed5b0fd966e1bfa31dced12b7244c49
wangxidi <wxd237@gmail.com>
2014-05-16 12:45:03 +0800

    t3

---
+++ protected/views/books/adminl.php
@@ -0,0 +1,58 @@
+<?php
+/* @var $this BooksController */
+/* @var $model Books */
+
+$this->breadcrumbs=array(
+	'Books'=>array('index'),
+	'Manage',
+);
+
+
+
+Yii::app()->clientScript->registerScript('search', "
+$('.search-button').click(function(){
+	$('.search-form').toggle();
+	return false;
+});
+$('.search-form form').submit(function(){
+	$('#books-grid').yiiGridView('update', {
+		data: $(this).serialize()
+	});
+	return false;
+});
+");
+?>
+
+<h1><?echo $mytitle;?></h1>
+
+
+
+
+<?php $this->widget('zii.widgets.grid.CGridView', array(
+	'id'=>'books-grid',
+	'dataProvider'=>$dataProvider,
+	'columns'=>array(
+		'bookid',
+		'categoryid',
+		'bookname',
+		'luserid',
+		'detail',	
+		/*
+		'commend',
+		'lendtime',
+		'buserid',
+		'borrowtime',
+		'author',
+		'public',
+		'number',
+		*/
+		array(
+			'class'=>'CButtonColumn',
+		),
+		array(
+			'class'=>'CLinkColumn',
+			'label'=>'收回',
+			'url'=>Yii->app()->createUrl('/books/back',array('id'=>$data->bookid)),
+			),
+	),
+)); ?>
